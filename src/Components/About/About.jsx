f3c4500fd95344e3bdf7231b9bae0628741464ca
ajaysharma200 <sharmaajay212003@gmail.com>
2025-07-01 14:37:06 +0530

    Fix case-sensitive image import paths

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './About.css'
-import about_img from '../../Assets/about.png'
-import play_icon from '../../Assets/play-icon.png' 
+import about_img from '../../assets/about.png'
+import play_icon from '../../assets/play-icon.png' 
 
 const About = ({setPlayState}) => {
   return (
